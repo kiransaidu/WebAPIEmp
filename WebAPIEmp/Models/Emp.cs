ccb5131a9e6ce109898763539f2fbdb51662c2d0
kiran <kiransaidu179@gmail.com>
2024-02-07 14:31:31 +0530

    Add project files.

---
+++ WebAPIEmp/Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIEmp.Models
+{
+    [Table("Emp")]
+    public class Emp
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Salary { get; set; }
+        public DateTime DOJ{ get; set; }
+    }
+}
